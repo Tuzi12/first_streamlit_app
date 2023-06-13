6bb622cb4860e0d69d3e89f4036ee7faed579b90
Tuzi12 <jianli12@gmail.com>
2023-06-13 13:05:05 +0100

    Update streamlit_app.py
    Add text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
-import streamlit
-streamlit.title("My parents new healthy dinner")
+import streamlit as st
+st.title("My parents new healthy dinner")
+st.header("Breakfast Menu")
+st.text('Omega 3 & Blueburry Oatmeal',
+         'Kale, Spinach & Rocket Smoothie',
+         'Hard-boiled Freerange Egg')
+
+
