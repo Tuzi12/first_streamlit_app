531bfc596eb3a5933c1c2c6a49f396b4f99f9295
Tuzi12 <jianli12@gmail.com>
2023-06-13 13:06:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 st.title("My parents new healthy dinner")
 st.header("Breakfast Menu")
-st.text('Omega 3 & Blueburry Oatmeal',
-         'Kale, Spinach & Rocket Smoothie',
-         'Hard-boiled Freerange Egg')
+st.text('Omega 3 & Blueburry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-boiled Freerange Egg')
 
 
